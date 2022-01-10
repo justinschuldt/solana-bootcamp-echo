d2ebdf95a2159208abd7d61c7a45b11935b1cb64
Justin Schuldt <justinschuldt@gmail.com>
2022-01-10 16:29:54 -0600

    echo working

---
+++ js/index.js
@@ -0,0 +1,102 @@
+
+
+const {
+  Connection,
+  sendAndConfirmTransaction,
+  Keypair,
+  Transaction,
+  SystemProgram,
+  PublicKey,
+  TransactionInstruction,
+} = require("@solana/web3.js");
+
+const lastDeploy = require('../last-deploy.json');
+
+const { programId: programAddress} = lastDeploy
+
+const BN = require("bn.js");
+
+const URL = "http://127.0.0.1:8899"
+// const URL = "https://api.devnet.solana.com/"
+
+const main = async () => {
+  var args = process.argv.slice(2);
+  // const programId = new PublicKey(args[0]);
+  const echo = args[0];
+  console.log('address', programAddress)
+  const programId = new PublicKey(programAddress);
+
+  const connection = new Connection(URL);
+
+  const feePayer = new Keypair();
+  const echoBuffer = new Keypair();
+
+  console.log("Requesting Airdrop of 1 SOL...");
+  await connection.requestAirdrop(feePayer.publicKey, 2e9);
+  console.log("Airdrop received");
+
+  console.log("going to echo: ", echo)
+
+  let createIx = SystemProgram.createAccount({
+    fromPubkey: feePayer.publicKey,
+    newAccountPubkey: echoBuffer.publicKey,
+    /** Amount of lamports to transfer to the created account */
+    lamports: await connection.getMinimumBalanceForRentExemption(echo.length),
+    /** Amount of space in bytes to allocate to the created account */
+    space: echo.length,
+    /** Public key of the program to assign as the owner of the created account */
+    programId: programId,
+  });
+
+  const idx = Buffer.from(new Uint8Array([0]));
+  const messageLen = Buffer.from(new Uint8Array((new BN(echo.length)).toArray("le", 4)));
+  const message = Buffer.from(echo, "ascii");
+
+  let echoIx = new TransactionInstruction({
+    keys: [
+      {
+        pubkey: echoBuffer.publicKey,
+        isSigner: false,
+        isWritable: true,
+      },
+    ],
+    programId: programId,
+    data: Buffer.concat([idx, messageLen, message]),
+  });
+
+  let tx = new Transaction();
+  tx.add(createIx).add(echoIx);
+
+let txid = undefined
+  try {
+    txid = await sendAndConfirmTransaction(
+      connection,
+      tx,
+      [feePayer, echoBuffer],
+      {
+        skipPreflight: true,
+        preflightCommitment: "confirmed",
+        confirmation: "confirmed",
+      }
+    );
+
+  } catch (error) {
+    console.log(error)
+  }
+  let explorerCluster = "custom&customUrl=http://localhost:8899"
+  if (URL.includes("devnet")) {
+    explorerCluster = "devnet"
+  }
+  console.log(`https://explorer.solana.com/tx/${txid}?cluster=${explorerCluster}`);
+
+  data = (await connection.getAccountInfo(echoBuffer.publicKey)).data;
+  console.log("Echo Buffer Text:", data.toString());
+};
+
+main()
+  .then(() => {
+    console.log("Success");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
