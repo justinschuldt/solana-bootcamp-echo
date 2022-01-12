9df06fa2d0e6c5ce2c8677cfd61c6bb20a0baf3d
Justin Schuldt <justinschuldt@gmail.com>
2022-01-12 17:24:54 -0600

    1: works, without instruction data

--- js/authEcho.ts
+++ js/authEcho.ts
@@ -9,71 +9,100 @@ import {
   PublicKey,
   TransactionInstruction,
 } from "@solana/web3.js"
-import { readFile } from "fs"
+
+import * as fs from "fs"
+const { readFile } = fs.promises
 
 const lastDeploy = require('../last-deploy.json');
 
 const { programId: programAddress} = lastDeploy
 
-const  BN = require("bn.js")
+import BN = require("bn.js")
 
 const rpcUrl = "http://127.0.0.1:8899"
 // const URL = "https://api.devnet.solana.com/"
 
-const main = async () => {
-  var args = process.argv.slice(2);
-  // const programId = new PublicKey(args[0]);
-  const echo = args[0];
-  console.log('address', programAddress)
-  const programId = new PublicKey(programAddress);
+const explorerLink = (txid: string): string => {
+  let explorerCluster = "custom&customUrl=http://localhost:8899"
+  if (rpcUrl.includes("devnet")) {
+    explorerCluster = "devnet"
+  }
+  return `https://explorer.solana.com/tx/${txid}?cluster=${explorerCluster}`
+}
 
-  const connection = new Connection(rpcUrl);
+const initialize = (authBuffer: PublicKey,  authority: PublicKey,  programId: PublicKey, buffer_seed: number, buffer_size: number) => {
+  const idx = Buffer.from(new Uint8Array([1]));
 
-  const feePayer = new Keypair();
-  const echoBuffer = new Keypair();
+  let seedSizeLen = Buffer.from(new Uint8Array((new BN(buffer_size)).toArray("le")))
+  let seedBuf = new BN(buffer_seed).toBuffer()
 
-  console.log("Requesting Airdrop of 1 SOL...");
-  await connection.requestAirdrop(feePayer.publicKey, 2e9);
-  console.log("Airdrop received");
+  let bufferSizeLen = Buffer.from(new Uint8Array((new BN(buffer_seed)).toArray("le")))
+  let sizeBuf = new BN(buffer_size).toBuffer()
 
-  console.log("going to echo: ", echo)
-
-  let createIx = SystemProgram.createAccount({
-    fromPubkey: feePayer.publicKey,
-    newAccountPubkey: echoBuffer.publicKey,
-    /** Amount of lamports to transfer to the created account */
-    lamports: await connection.getMinimumBalanceForRentExemption(echo.length),
-    /** Amount of space in bytes to allocate to the created account */
-    space: echo.length,
-    /** Public key of the program to assign as the owner of the created account */
-    programId: programId,
-  });
-
-  const idx = Buffer.from(new Uint8Array([0]));
-  const messageLen = Buffer.from(new Uint8Array((new BN(echo.length)).toArray("le", 4)));
-  const message = Buffer.from(echo, "ascii");
-
-  let echoIx = new TransactionInstruction({
+  let i = new TransactionInstruction({
     keys: [
       {
-        pubkey: echoBuffer.publicKey,
+        pubkey: authBuffer,
         isSigner: false,
         isWritable: true,
       },
+      {
+        pubkey: authority,
+        isSigner: true,
+        isWritable: true,
+      },
+      {
+        pubkey: SystemProgram.programId,
+        isSigner: false,
+        isWritable: false,
+      },
     ],
+    data: idx,
     programId: programId,
-    data: Buffer.concat([idx, messageLen, message]),
   });
+  console.log(i)
+  return i
+};
+
+const write = (authBuffer: PublicKey,  authority: PublicKey,  programId: PublicKey, message: string) => {
+  const idx = Buffer.from(new Uint8Array([2]));
+  const messageLen = Buffer.from(new Uint8Array((new BN(message.length)).toArray("le", 4)));
+  const messageBuf = Buffer.from(message, "ascii");
+  
+  return new TransactionInstruction({
+    keys: [
+      {
+        pubkey: authBuffer,
+        isSigner: false,
+        isWritable: true,
+      },
+      {
+        pubkey: authority,
+        isSigner: true,
+        isWritable: false,
+      },
+    ],
+    data: Buffer.concat([idx, messageLen, messageBuf]),
+    programId: programId,
+  });
+};
+const second = async (connection: Connection, programId: PublicKey, authBuffer: PublicKey, feePayer: Keypair, authorized: Keypair, message: string): Promise<string> => {
 
   let tx = new Transaction();
-  tx.add(createIx).add(echoIx);
-
-let txid = undefined
+  let signers = [feePayer, authorized]
+  const writeInstruction = write(
+    authBuffer,
+    authorized.publicKey,
+    programId,
+    message
+  )
+  tx.add(writeInstruction);
+  let txid = undefined
   try {
     txid = await sendAndConfirmTransaction(
       connection,
       tx,
-      [feePayer, echoBuffer],
+      signers,
       {
         skipPreflight: true,
         preflightCommitment: "confirmed",
@@ -82,19 +111,191 @@ let txid = undefined
     );
 
   } catch (error) {
-    console.log(error)
+    console.error(error)
+    throw error
   }
-  let explorerCluster = "custom&customUrl=http://localhost:8899"
-  if (rpcUrl.includes("devnet")) {
-    explorerCluster = "devnet"
+  return txid
+}
+
+const main = async () => {
+  var args = process.argv.slice(2);
+  // const programId = new PublicKey(args[0]);
+  const echo = args[0];
+  const programId = new PublicKey(programAddress);
+  console.log('programId', programId.toBase58())
+
+  const buffer_seed = 123456
+  const buffer_size = 124
+
+  const connection = new Connection(rpcUrl);
+
+  const feePayer = new Keypair();
+  console.log("feePayer pubKey:", feePayer.publicKey.toBase58())
+
+  const authorized = new Keypair();
+  console.log("authorized pubKey:", authorized.publicKey.toBase58())
+
+  // use authorized account from local filesystem keypair
+  // let secretKeyString = await readFile('./auth-keypair.json',  "utf8",);
+  // console.log("Loaded Keypair from ./auth-keypair.json");
+  // const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
+  // const authorized = Keypair.fromSecretKey(secretKey);
+  // console.log("authorized pubKey:", authorized.publicKey.toBase58())
+
+  // const auth = PublicKey.createWithSeed(
+  //   authorized.publicKey,
+  //   String(buffer_seed),
+  //   programId
+  // )
+
+  // let authBuffer = await PublicKey.createProgramAddress(
+  //   [
+  //     Buffer.from(`authority`, "ascii"),
+  //     authorized.publicKey.toBuffer(),
+  //     new Uint8Array(new BN(buffer_seed).toArray("le", 4))
+  // ],
+  // programId
+  // )
+  
+  console.log("authority ", Buffer.from(`authority`))
+  console.log("authorized.publicKey.toBuffer() ", authorized.publicKey.toBuffer())
+  console.log("new Uint8Array((new BN(buffer_seed)).toArray('le'))", new Uint8Array((new BN(buffer_seed)).toArray("le", 8)))
+  let [authBuffer, bump_seed] = await PublicKey.findProgramAddress(
+    [
+        Buffer.from(`authority`),
+        authorized.publicKey.toBuffer(),
+        // new BN(buffer_seed).toBuffer(),
+        new Uint8Array(new BN(buffer_seed).toArray("le", 8))
+    ],
+    programId
+  )
+  console.log("authBuffer pubKey:", authBuffer.toBase58())
+  console.log('bump_seed', bump_seed)
+
+  console.log("Requesting Airdrop of 1 SOL for feePayer");
+  await connection.requestAirdrop(feePayer.publicKey, 2e9);
+  console.log("Requesting Airdrop of 1 SOL for authorized");
+  await connection.requestAirdrop(authorized.publicKey, 2e9);
+  console.log("Airdrops received");
+
+
+  let tx = new Transaction();
+
+  // let createIx = SystemProgram.createAccount({
+  //   fromPubkey: authorized.publicKey,  // feepayer
+  //   newAccountPubkey: authBuffer,      // 
+  //   /** Amount of lamports to transfer to the created account */
+  //   lamports: await connection.getMinimumBalanceForRentExemption(buffer_size),
+  //   /** Amount of space in bytes to allocate to the created account */
+  //   space: buffer_size,
+  //   /** Public key of the program to assign as the owner of the created account */
+  //   programId: programId,
+  // });
+  // tx.add(createIx)
+
+  // const authKey = (await PublicKey.findProgramAddress(
+  //   [echoBuffer.publicKey.toBuffer()],
+  //   programId
+  // ))[0];
+
+  // const authBufferKey = (await PublicKey.findProgramAddress(
+  //   [feePayer.publicKey.toBuffer(), echoBuffer.publicKey.toBuffer()],
+  //   programId
+  // ))[0];
+  let signers = [feePayer, authorized]
+
+  let authBuffData = await connection.getAccountInfo(authBuffer)
+  if (!authBuffData) {
+    console.log("no authBuffer account found, going to initialize");
+    const initializeIx = initialize(
+      authBuffer,
+      authorized.publicKey,
+      programId,
+      buffer_seed,
+      buffer_size,
+    );
+    tx.add(initializeIx);
   }
-  console.log(`https://explorer.solana.com/tx/${txid}?cluster=${explorerCluster}`);
+
+  console.log("Writing message from the authorized account");
+  const writeInstruction = write(
+    authBuffer,
+    authorized.publicKey, // authKey,
+    programId,
+    echo
+  )
+  tx.add(writeInstruction);
+  signers.push(authorized)
+
+  // const idx = Buffer.from(new Uint8Array([0]));
+  // const messageLen = Buffer.from(new Uint8Array((new BN(echo.length)).toArray("le", 4)));
+  // const message = Buffer.from(echo, "ascii");
+
+  // let echoIx = new TransactionInstruction({
+  //   keys: [
+  //     {
+  //       pubkey: echoBuffer.publicKey,
+  //       isSigner: false,
+  //       isWritable: true,
+  //     },
+  //   ],
+  //   programId: programId,
+  //   data: Buffer.concat([idx, messageLen, message]),
+  // });
+  // tx.add(echoIx);
+
+let txid = undefined
+  try {
+    txid = await sendAndConfirmTransaction(
+      connection,
+      tx,
+      signers,
+      {
+        skipPreflight: true,
+        preflightCommitment: "confirmed",
+        commitment: "confirmed",
+      }
+    );
+
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+
+  console.log("first write", explorerLink(txid))
 
   setTimeout(async ()=> {
-    let data = (await connection.getAccountInfo(echoBuffer.publicKey, "confirmed") || {}).data
-    console.log("Echo Buffer Text:", data?.toString());
-  },4000)
-  
+    const getEchoData = async (connection: Connection, account: PublicKey): Promise<string> => {
+      let data = (await connection.getAccountInfo(authBuffer, "confirmed") || {}).data
+      // console.log("Echo Buffer Text:", data?.slice(9).toString());
+      return data?.slice(9).toString() || ""
+    }
+    const firstWriteData = await getEchoData(connection, authBuffer)
+    console.log("first write data: ", firstWriteData)
+    try {
+      // should succeed
+      let txid = await second(connection, programId, authBuffer, feePayer, authorized, "second write worked!")
+      console.log("second write", explorerLink(txid))
+      setTimeout(async () => {
+        const writeData = await getEchoData(connection, authBuffer)
+        console.log("second write data: ", writeData)
+      }, 4000)
+    } catch {
+
+    }
+    try {
+      // should fail
+      let txid = await second(connection, programId, authBuffer, feePayer, feePayer, "this should fail")
+      console.log("failed write", explorerLink(txid))
+    } catch(error) {
+      console.log("third write err", error)
+      setTimeout(async () => {
+        const writeData = await getEchoData(connection, authBuffer)
+        console.log("third write data: ", writeData)
+      }, 4000)
+    }
+
+  },6000)
 };
 
 main()
--- program/src/processor.rs
+++ program/src/processor.rs
@@ -1,20 +1,36 @@
-use borsh::{BorshDeserialize};
+use borsh::{BorshDeserialize, BorshSerialize};
 
 use solana_program::{
-    entrypoint::ProgramResult, msg, program_error::ProgramError,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
     account_info::next_account_info,
     pubkey::Pubkey,
+    system_instruction,
+    program::{invoke_signed},
+    system_program::ID as SYSTEM_PROGRAM_ID,
+    sysvar::{rent::Rent, Sysvar},
 };
 use solana_program::account_info::AccountInfo;
 
 use crate::error::EchoError;
 use crate::instruction::EchoInstruction;
+use crate::state::AuthorizedBufferHeader;
+
+pub fn assert_with_msg(statement: bool, err: ProgramError, msg: &str) -> ProgramResult {
+    if !statement {
+        msg!(msg);
+        Err(err)
+    } else {
+        Ok(())
+    }
+}
 
 pub struct Processor {}
 
 impl Processor {
     pub fn process_instruction(
-        _program_id: &Pubkey,
+        program_id: &Pubkey,
         accounts: &[AccountInfo],
         instruction_data: &[u8],
     ) -> ProgramResult {
@@ -22,7 +38,7 @@ impl Processor {
             .map_err(|_| ProgramError::InvalidInstructionData)?;
 
         match instruction {
-            EchoInstruction::Echo { data: message_vec } => {
+            EchoInstruction::Echo { data: mut message_vec } => {
                 msg!("Instruction: Echo");
 
                 let accounts_iter = &mut accounts.iter();
@@ -37,29 +53,154 @@ impl Processor {
                 // trim if needed.
                 let len = user_account.data_len();
                 let message_len = message_vec.len();
-                let mut trimmed_message = message_vec; 
                 if message_len > len {
-                    trimmed_message = trimmed_message[0..len].to_vec();
+                    msg!("trimming message to length {}", len);
+                    message_vec.truncate(len);
+                } else if len > message_len {
+                    msg!("length of data is longer than message length.");
+                    message_vec.resize(len, 0);
                 }
 
                 // write message to account
                 let buffer = &mut user_account.data.borrow_mut();
-                buffer.copy_from_slice(&trimmed_message);
+                buffer.copy_from_slice(&message_vec);
 
                 msg!("message written to account");
 
                 Ok(())
             }
             EchoInstruction::InitializeAuthorizedEcho {
-                buffer_seed: _,
-                buffer_size: _,
+                // buffer_seed,
+                // buffer_size,
             } => {
-                msg!("Instruction: InitializeAuthorizedEcho");
-                Err(EchoError::NotImplemented.into())
+                let buffer_seed: u64 = 123456;
+                let buffer_size: usize = 124;
+                msg!("Instruction: InitializeAuthorizedEcho {} {}",buffer_seed, buffer_size);
+
+                let accounts_iter = &mut accounts.iter();
+
+                let authorized_buffer_ai = next_account_info(accounts_iter)?;
+                msg!("authorized_buffer_ai.key {}", authorized_buffer_ai.key);
+                let authority = next_account_info(accounts_iter)?;
+                msg!("authority.key {}", authority.key);
+                let system_program = next_account_info(accounts_iter)?;
+
+                msg!("authority {:?}", b"authority");
+                msg!("&buffer_seed.to_le_bytes() {:?}", buffer_seed.to_le_bytes());
+                let (authorized_buffer_key, bump_seed) = Pubkey::find_program_address(
+                    &[
+                        b"authority",
+                        authority.key.as_ref(),
+                        &buffer_seed.to_le_bytes()
+                    ],
+                    program_id,
+                );
+                msg!("authorized_buffer_key {}", authorized_buffer_key);
+                msg!("bump_seed {}", bump_seed);
+
+        
+                let auth_pubkey = Pubkey::create_program_address(
+                    &[
+                        b"authority",
+                        authority.key.as_ref(),
+                        &buffer_seed.to_le_bytes(),
+                        &[bump_seed]
+                    ], 
+                    program_id
+                )?;
+                msg!("created program address: {}", auth_pubkey);
+
+                if *authorized_buffer_ai.key != authorized_buffer_key {
+                    // allocated key does not match the derived address
+                    return Err(ProgramError::InvalidArgument);
+                }
+        
+                // invoke: use when no PDAs
+                // invoke_signed: use when PDAs sign
+                msg!("going to invoke_signed");
+                let res = invoke_signed(
+                    &system_instruction::create_account(
+                        authority.key,
+                        &authorized_buffer_key,
+                        Rent::get()?.minimum_balance(buffer_size),
+                        buffer_size.try_into().unwrap(),
+                        program_id,
+                    ),
+                    &[authority.clone(), authorized_buffer_ai.clone(), system_program.clone()],
+                    &[&[b"authority", authority.key.as_ref(), &buffer_seed.to_le_bytes(), &[bump_seed]]],
+                );
+                msg!("invoke_signed res {:?}", res);
+    
+                assert_with_msg(
+                    *system_program.key == SYSTEM_PROGRAM_ID,
+                    ProgramError::InvalidArgument,
+                    "Invalid passed in for system program",
+                )?;
+
+                let mut auth_buff_header = AuthorizedBufferHeader::try_from_slice(&authorized_buffer_ai.data.borrow()[0..9])?;
+                auth_buff_header.bump_seed = bump_seed;
+                auth_buff_header.buffer_seed = buffer_seed;
+                auth_buff_header.serialize(&mut *authorized_buffer_ai.data.borrow_mut())?;
+
+                msg!("Done initalizing AuthorizedEcho");
+                Ok(())
             }
-            EchoInstruction::AuthorizedEcho { data: _ } => {
+            EchoInstruction::AuthorizedEcho { data: mut message_vec } => {
                 msg!("Instruction: AuthorizedEcho");
-                Err(EchoError::NotImplemented.into())
+                let accounts_iter = &mut accounts.iter();
+
+                let authorized_buffer_ai = next_account_info(accounts_iter)?;
+                msg!("authorized_buffer_ai.key {}", authorized_buffer_ai.key);
+                let authority = next_account_info(accounts_iter)?;
+                msg!("authority.key {}", authority.key);
+
+                let auth_buff_header = AuthorizedBufferHeader::try_from_slice(&authorized_buffer_ai.data.borrow()[0..9])?;
+                let bump_seed = auth_buff_header.bump_seed;
+                let buffer_seed = auth_buff_header.buffer_seed;
+                msg!("bump_seed {}", bump_seed);
+                msg!("buffer_seed {}", buffer_seed);
+
+                let auth_pubkey = Pubkey::create_program_address(
+                    &[
+                        b"authority",
+                        authority.key.as_ref(),
+                        &buffer_seed.to_le_bytes(),
+                        &[bump_seed]
+                    ], 
+                    program_id
+                )?;
+
+                msg!("authorized_buffer_ai.key {}", authorized_buffer_ai.key);
+                msg!("derived auth_pubkey {}", auth_pubkey);
+
+                assert_with_msg(
+                    *authorized_buffer_ai.key == auth_pubkey,
+                    ProgramError::InvalidArgument,
+                    "Invalid PDA seeds for authority",
+                )?;
+
+
+                let message_buffer = &mut authorized_buffer_ai.data.borrow_mut()[9..];
+                let message_buffer_len = message_buffer.len();
+                msg!("message_buffer_len {}", message_buffer_len);
+
+                let message_len = message_vec.len();
+                msg!("message_len {}", message_len);
+
+                if message_len > message_buffer_len {
+                    msg!("truncating message to length {}", message_buffer_len);
+                    message_vec.truncate(message_buffer_len);
+
+                } else if message_buffer_len > message_len {
+                    msg!("length of existing message is longer than new message length.");
+
+                    message_vec.resize(message_buffer_len, 0);
+                }
+                
+                message_buffer.copy_from_slice(&message_vec);
+                msg!("wrote message_vec to message_buffer!");
+
+                Ok(())
             }
             EchoInstruction::InitializeVendingMachineEcho {
                 price: _,
